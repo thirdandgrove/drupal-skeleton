76608bc9b7d408c365dea744b8997f58a1ed14bc
Anthony Gonzales <anthony@thirdandgrove.com>
2015-05-07 22:12:56 -0400

    Create basic theme.info file. Create template.php with system css resets. Create a master styles SCSS file importing the base file including a box-sizing reset. Add Compass config.rb to run Compass. Create a Gemfile and Gemfile.lock to set Compass dependency.

---
+++ docroot-goodies/skeleton/template.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Main theme overrides and hooks for Skeleton Theme.
+ */
+
+/**
+ * Implements hook_css_alter().
+ */
+function skeleton_css_alter(&$css) {
+
+  // Remove Drupal core css.
+  // Credit to @_fmachine: http://friendlymachine.net/posts/how-remove-core-and-module-css-drupal-7-theme
+
+  $exclude = array(
+  'modules/node/node.css' => FALSE,
+  'modules/system/admin.css' => FALSE,
+  'modules/system/maintenance.css' => FALSE,
+  'modules/system/system.css' => FALSE,
+  'modules/system/system.admin.css' => FALSE,
+  'modules/system/system.base.css' => FALSE,
+  'modules/system/system.maintenance.css' => FALSE,
+  'modules/system/system.messages.css' => FALSE,
+  'modules/system/system.menus.css' => FALSE,
+  'modules/system/system.theme.css' => FALSE,
+
+  // Remove contrib module CSS
+  drupal_get_path('module', 'views') . '/css/views.css' => FALSE, );
+  $css = array_diff_key($css, $exclude);
+}
