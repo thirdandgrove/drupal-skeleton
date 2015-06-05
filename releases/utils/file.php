dd395fe73986b1f7ac620a7d512d4bb3138d8b4a
Josh Fremer <josh@thirdandgrove.com>
2015-06-05 15:59:51 -0400

    Update variable names for clarity.

--- releases/utils/file.php
+++ releases/utils/file.php
@@ -11,16 +11,16 @@
  * to the same destination directory. Set $retain_path = TRUE to retain the old
  * paths relative to the new $destination_base_path.
  *
- * @param string $file_base_path
+ * @param string $source_base_path
  *   The path to the directory containing the files.
  * @param string $destination_base_path
  *   The destination directory prefix.
  * @param boolean $retain_path
- *   If TRUE, any path after $file_base_path in the source will be retained on
+ *   If TRUE, any path after $source_base_path in the source will be retained on
  *   the new copy.
  */
-function tag_import_files($file_base_path, $destination_base_path = '', $retain_path = FALSE) {
-  $files = file_scan_directory($file_base_path, '/[.*\.jpg]|[.*\.png]|[.*\.gif]|[.*\.pdf]$/');
+function tag_import_files($source_base_path, $destination_base_path = '', $retain_path = FALSE) {
+  $files = file_scan_directory($source_base_path, '/[.*\.jpg]|[.*\.png]|[.*\.gif]|[.*\.pdf]$/');
   // Sorting what is returned helps ensure that the file order is the same when
   // run on different systems. The configuration of local systems can cause the
   // ordering to be different.
@@ -35,7 +35,7 @@ function tag_import_files($file_base_path, $destination_base_path = '', $retain_
       // Break apart the file uri to find the relative path.
       $file_path = $file->uri;
       // We don't want the base path.
-      $file_path = str_replace($file_base_path . '/', '', $file_path);
+      $file_path = str_replace($source_base_path . '/', '', $file_path);
       // Discard the filename.
       $file_path_parts = explode('/', $file_path);
       array_pop($file_path_parts);
